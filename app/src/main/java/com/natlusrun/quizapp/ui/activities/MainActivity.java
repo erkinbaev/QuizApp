b0f5558d283e3a0444129a46fd7776f58388f64f
imgeek007 <natlusrun94@gmail.com>
2020-11-24 17:03:13 +0300

    main fragment layout is ready, but @todo: add shadow to the spinner, add some effect to botnav when select the icon, add mvvm method to change number

--- app/src/main/java/com/natlusrun/quizapp/ui/activities/MainActivity.java
+++ app/src/main/java/com/natlusrun/quizapp/ui/activities/MainActivity.java
@@ -1,13 +1,17 @@
 package com.natlusrun.quizapp.ui.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.natlusrun.quizapp.R;
+import com.natlusrun.quizapp.ui.adapters.NonSwipeViewPager;
 import com.natlusrun.quizapp.ui.adapters.ViewPagerAdapter;
 import com.natlusrun.quizapp.ui.fragments.history.HistoryFragment;
 import com.natlusrun.quizapp.ui.fragments.main.MainFragment;
@@ -23,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
     private MainFragment mainFragment;
     private List<Fragment> fragmentList;
 
+    private NonSwipeViewPager nonSwipeViewPager;
+
+    private final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,11 +39,34 @@ public class MainActivity extends AppCompatActivity {
         fragmentList = new ArrayList<>();
         fillFragments();
 
-        viewPager = findViewById(R.id.main_view_pager);
+        //viewPager = findViewById(R.id.main_view_pager);
+        nonSwipeViewPager = findViewById(R.id.main_view_pager);
         bottomNavigationView = findViewById(R.id.main_bottom_nav);
         bottomNavigationView.setItemIconTintList(null);
 
-        viewPager.setAdapter(new ViewPagerAdapter(fragmentList, getSupportFragmentManager()));
+        nonSwipeViewPager.setAdapter(new ViewPagerAdapter(fragmentList, getSupportFragmentManager()));
+        nonSwipeViewPager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+            }
+        });
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.main_nav) {
+                    nonSwipeViewPager.setCurrentItem(0);
+                } else if(item.getItemId() == R.id.history_nav){
+                    nonSwipeViewPager.setCurrentItem(1);
+                }else if(item.getItemId() == R.id.settings_nav){
+                    nonSwipeViewPager.setCurrentItem(2);
+                }
+                return true;
+            }
+        });
+
         mainFragment = new MainFragment();
 //        getSupportFragmentManager()
 //                .beginTransaction()
@@ -43,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
 //                .commit();
     }
 
-    private void fillFragments(){
+    private void fillFragments() {
         fragmentList.add(new MainFragment());
         fragmentList.add(new HistoryFragment());
         fragmentList.add(new SettingsFragment());
---
+++ app/src/main/java/com/natlusrun/quizapp/ui/adapters/NonSwipeViewPager.java
@@ -0,0 +1,38 @@
+package com.natlusrun.quizapp.ui.adapters;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
+
+public class NonSwipeViewPager extends ViewPager {
+
+    private boolean enabled;
+    public NonSwipeViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if(this.enabled){
+            return super.onTouchEvent(ev);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if(this.enabled) {
+            return super.onInterceptTouchEvent(ev);
+        }
+        return false;
+    }
+
+    public void setPagingEnabled(boolean enabled){
+        this.enabled = enabled;
+    }
+}
